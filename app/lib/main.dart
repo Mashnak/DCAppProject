8d1a69efac4c7b0d7d5b383002956686503e2143
TimurBahadir <Timur.Bahadir@gmail.com>
2018-04-16 15:25:24 +0200

    Added basic Song-View example.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,10 +3,37 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  Widget _buildInfoSection(String sectionName, String sectionValue) {
+    return new Container(
+      padding: const EdgeInsets.all(12.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: new Text(
+                    sectionName,
+                    style: new TextStyle(color: Colors.grey),
+                  ),
+                ),
+                new Text(sectionValue,
+                    style: new TextStyle(
+                        fontSize: 20.0, fontWeight: FontWeight.bold))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Song View',
+      title: 'Song Finder',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -27,11 +54,24 @@ class MyApp extends StatelessWidget {
                 )
               ],
             ),
-            title: new Text('Song Title'),
+            title: new Text('Legend Has It - Run the Jewels'),
           ),
           body: new TabBarView(
             children: [
-              new Icon(Icons.info),
+              new ListView(
+                children: <Widget>[
+                  new Image.asset(
+                    'images/lake.jpg',
+                    height: 240.0,
+                    fit: BoxFit.cover,
+                  ),
+                  _buildInfoSection('Song Title', 'Legend Has It'),
+                  _buildInfoSection('Album Name', 'Run The Jewels 3'),
+                  _buildInfoSection('Artist Name', 'Run The Jewels'),
+                  _buildInfoSection('Label', 'Run The Jewels, Inc.'),
+                  _buildInfoSection('Release Date', '2016'),
+                ],
+              ),
               new Icon(Icons.music_note),
               new Icon(Icons.comment)
             ],
