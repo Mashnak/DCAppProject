e4550ec3905b5682270df00ec634f568b82e770c
Markus <Markus.Schmidgall@googlemail.com>
2018-04-25 02:33:15 +0200

    Trying to fix anything with my web app!!!

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'app-cards',
+    templateUrl: './Cards.html',
+    styleUrls: ['./app.component.css', '../../node_modules/bootstrap/dist/css/bootstrap.css']
+  }
+)
+export class AppComponent {
+  title = 'Song example';
+}
