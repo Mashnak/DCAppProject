9deed325c22aead01754bff39bd093be38213e4a
Markus <Markus.Schmidgall@googlemail.com>
2018-04-25 03:37:34 +0200

    Styling things

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Song example';
+  songtitle = 'Gangnam Style';
+  artist = 'Psy';
+  searchrequest = 'Korean Music';
 }
