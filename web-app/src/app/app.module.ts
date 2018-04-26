5db2a01fb69e2377b57ed5c7b04102a0ccf7f110
Markus Schmidgall <Markus.Schmidgall@googlemail.com>
2018-04-27 00:58:37 +0200

    Outsorced html code to several modules

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,17 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-
+import {SongsearchComponent} from './songsearch.component';
+import {SongitemComponent} from './songitem.component';
+import {DetailpageComponent} from './detailpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    SongsearchComponent, SongitemComponent, DetailpageComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [SongsearchComponent]
 })
 export class AppModule {
 }
---
+++ web-app/src/app/songsearch.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-songsearch',
+  templateUrl: './songsearch.component.html',
+  styleUrls: ['./songsearch.component.css']
+})
+export class SongsearchComponent {
+  title = 'Song Search Example';
+}
