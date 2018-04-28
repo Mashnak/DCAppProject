76bcb974ca13b35c6c36ff5c44a691c873d83690
Markus Schmidgall <Markus.Schmidgall@googlemail.com>
2018-04-28 05:09:27 +0200

    Added post method for songs. Still need to fix the RootURL

---
+++ web-app/src/app/song.ts
@@ -0,0 +1,11 @@
+export interface Song {
+  number: String;
+  title: String;
+  length: String;
+  releaseDate: String;
+  lyrics: String;
+  genres: String;
+  album: String;
+  artist: String;
+}
+
--- web-app/src/app/songsearch.component.ts
+++ web-app/src/app/songsearch.component.ts
@@ -1,5 +1,10 @@
 import {Component} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Song} from './song';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/retry';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-songsearch',
@@ -8,16 +13,38 @@ import {HttpClient} from '@angular/common/http';
 })
 
 export class SongsearchComponent {
-  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com'; //Fix the ROOT_URL to the correct springboot URL then get and post should work
 
-  posts: any;
+  posts: Observable<any>;
+  newSong: Observable<any>;
 
   constructor(private http: HttpClient) {
   }
 
   title = 'Song Search Example';
 
-  getPosts() {
-    this.posts = this.http.get(this.ROOT_URL + '/posts');
+  getSong() {
+    let params = new HttpParams().set('userId', '1');
+
+    this.posts = this.http.get<Song[]>(this.ROOT_URL + '/posts', {params});
+  }
+
+  postSong() {
+    const data: Song = {
+      number: '507f191e810c19729de860ea',
+      title: 'Legend Has It',
+      length: '3:25',
+      releaseDate: '2016-12-24',
+      lyrics: 'Her what I say, whe are the business today...',
+      genres: 'Hip Hop/Rap',
+      album: 'Run The Jewels 3',
+      artist: 'Run The Jewels'
+    };
+    this.newSong = this.http.post(this.ROOT_URL + 'posts', data)
+      .retry(3)
+      .catch(err => {
+        console.log(err);
+        return Observable.of(err);
+      });
   }
 }
