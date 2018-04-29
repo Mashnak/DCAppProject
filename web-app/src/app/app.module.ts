0ebae867f1c9eff4764bf9393882df70b59c84cc
Markus Schmidgall <Markus.Schmidgall@googlemail.com>
2018-04-29 20:08:16 +0200

    Added Documents for Part2 DesignMS

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -14,7 +14,7 @@ import {HttpClientModule} from '@angular/common/http';
     BrowserModule, routing,  HttpClientModule
   ],
   providers: [],
-  bootstrap: [SongsearchComponent] // Html entry point after index.html
+  bootstrap: [SongsearchComponent]
 })
 export class AppModule {
 }
