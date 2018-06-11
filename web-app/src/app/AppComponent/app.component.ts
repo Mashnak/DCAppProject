f4573a10427b64deea946ca54817cf97b197a670
Markus Schmidgall <Markus.Schmidgall@googlemail.com>
2018-06-11 12:07:31 +0200

    Add items to app.component.ts

--- web-app/src/app/AppComponent/app.component.ts
+++ web-app/src/app/AppComponent/app.component.ts
@@ -1,15 +1,14 @@
 import {Component} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
-import {Song} from '../song';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/retry';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/of';
+import {Song} from '../song';
 import {User} from '../user';
 import {Album} from '../album';
 import {Artist} from '../artist';
 import {Publisher} from '../publisher';
-
 
 @Component({
   selector: 'app-component',
@@ -22,17 +21,19 @@ export class AppComponent {
 
   songs: Observable<any>;
   users: Observable<any>;
-  album: Observable<any>;
-  artist: Observable<any>;
-  publisher: Observable<any>;
+  albums: Observable<any>;
+  artists: Observable<any>;
+  publishers: Observable<any>;
 
   constructor(private http: HttpClient) {
   }
 
   title = 'Song Search';
+
   user = {
     name: 'testuser'
   };
+
   song = {
     id: '507f191e810c19729de860ea',
     name: 'Legend Has It',
@@ -47,6 +48,37 @@ export class AppComponent {
     artists: ['Run The Jewels']
   };
 
+  album = {
+    id: '507f191e810c19729de860ea',
+    name: 'Run The Jewels 3',
+    releaseDate: '2016-12-24',
+    genres: ['Hip Hop/Rap'],
+    tags: [],
+    songs: ['Legend Has It'],
+    artists: ['Run The Jewels'],
+    publisher: ''
+  };
+
+  artist = {
+    id: '507f191e810c19729de860ea',
+    name: 'Run The Jewels',
+    genres: ['Hip Hop/Rap'],
+    tags: [],
+    songs: ['Legend Has It'],
+    publishers: [''],
+    albums: ['Run The Jewels 3']
+  };
+
+  publisher = {
+    id: '507f191e810c19729de860ea',
+    name: '',
+    genres: ['Hip Hop/Rap'],
+    tags: [],
+    songs: ['Legend Has It'],
+    albums: ['Run The Jewels 3'],
+    artists: 'Run The Jewels',
+  };
+
   getSong() {
     // let params = new HttpParams().set('userId', '1');
 
@@ -54,15 +86,15 @@ export class AppComponent {
   }
 
   getAlbum() {
-    this.album = this.http.get<Album[]>(this.ROOT_URL + '/album');
+    this.albums = this.http.get<Album[]>(this.ROOT_URL + '/album');
   }
 
   getArtist() {
-    this.artist = this.http.get<Artist[]>(this.ROOT_URL + '/artist');
+    this.artists = this.http.get<Artist[]>(this.ROOT_URL + '/artist');
   }
 
   getPublisher() {
-    this.publisher = this.http.get<Publisher[]>(this.ROOT_URL + '/publisher');
+    this.publishers = this.http.get<Publisher[]>(this.ROOT_URL + '/publisher');
   }
 
   getUser() {
