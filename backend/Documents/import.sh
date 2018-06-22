555f272f0a26d36369016f56e7bfcc5006a19128
Khaled <53558@hs-aalen.de>
2018-06-22 13:36:10 +0200

    - created 2 JSON-Files for n:m relationships between artist and album/song - removed capital letters from collectionnames in import.sh

--- backend/Documents/import.sh
+++ backend/Documents/import.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-mongoimport --db test --collection Genre --type json --file Documents/genre.json
-mongoimport --db test --collection Tag --type json --file Documents/tags.json
-mongoimport --db test --collection Album --type json --file Documents/album.json
-mongoimport --db test --collection Artist --type json --file Documents/artist.json
-mongoimport --db test --collection Song --type json --file Documents/bundesliga.json
-mongoimport --db test --collection Song --type json --file Documents/movies.json
-mongoimport --db test --collection Song --type json --file Documents/nationalhymne.json
+mongoimport --db test --collection album --type json --file Documents/album.json
+mongoimport --db test --collection artist --type json --file Documents/artist.json
+mongoimport --db test --collection song --type json --file Documents/bundesliga.json
+mongoimport --db test --collection song --type json --file Documents/movies.json
+mongoimport --db test --collection song --type json --file Documents/nationalhymne.json
+mongoimport --db test --collection artistsong --type json --file Documents/artistsong.json
+mongoimport --db test --collection artistalbum --type json --file Documents/artistalbum.json
