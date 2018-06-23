0a58e5f8f3f4996ffd3612d13c6962f6fb525df3
Bic Mag <bic.mag@t-online.de>
2018-06-23 17:42:36 +0200

    search now appends artists to the song

--- business_logic/src/main/java/de/teama/bl/Application.java
+++ business_logic/src/main/java/de/teama/bl/Application.java
@@ -2,6 +2,7 @@ package de.teama.bl;
 
 import com.mongodb.MongoClient;
 import de.teama.bl.data.*;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -247,9 +248,22 @@ public class Application implements ApplicationRunner {
         if (songResults.size() > 0){
             Songs[] songs = new Songs[songResults.size()];
             songs = songResults.toArray(songs);
-            result += songs[0].toString();
+            JSONObject artistSong = new JSONObject(songs[0]);
+            JSONArray artists = new JSONArray();
+            for (Artists artist :artistSongRepository.findBySong(songs[0].getName())){
+                artists.put(artist);
+            }
+            artistSong.put("artists", artists);
+            result += artistSong;
             for (int i = 1; i<songs.length; i++){
-                result += ("," + songs[i].toString());
+                result += ",";
+                artistSong = new JSONObject(songs[i]);
+                artists = new JSONArray();
+                for (Artists artist :artistSongRepository.findBySong(songs[0].getName())){
+                    artists.put(artist);
+                }
+                artistSong.put("artists", artists);
+                result += artistSong;
             }
         }
 
--- business_logic/src/main/java/de/teama/bl/ArtistSongRepository.java
+++ business_logic/src/main/java/de/teama/bl/ArtistSongRepository.java
@@ -1,12 +1,14 @@
 package de.teama.bl;
 
 import de.teama.bl.data.ArtistSongs;
+import de.teama.bl.data.Artists;
+import de.teama.bl.data.Songs;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ArtistSongRepository extends MongoRepository<ArtistSongs, String> {
-    List<ArtistSongs> findBySong(@Param("song") String song);
-    List<ArtistSongs> findByArtist(@Param("artist") String Artist);
+    List<Artists> findBySong(@Param("song") String song);
+    List<Songs> findByArtist(@Param("artist") String Artist);
 }
